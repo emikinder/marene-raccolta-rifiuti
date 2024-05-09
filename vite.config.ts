cd003fc8f38dcb3414c950ee45fbb1362772e698
Emiliano <emikinderk@gmail.com>
2024-05-09 13:33:19 +0200

    no-ticket/pwa

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "./",
     plugins: [
         react(),
         VitePWA({
@@ -16,7 +17,7 @@ export default defineConfig({
             },
             workbox: {
                 globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
-            }
+            },
         }),
     ],
     resolve: {
