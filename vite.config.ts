a83c75c648ff77b0a744cdd12a25f106c8908e3a
Emiliano <emikinderk@gmail.com>
2024-05-09 13:35:59 +0200

    no-ticket/pwa

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "./",
     plugins: [
         react(),
         VitePWA({
@@ -17,6 +16,41 @@ export default defineConfig({
             },
             workbox: {
                 globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+            },
+            manifest: {
+                name: "Marene Raccolta Rifiuti",
+                short_name: "Marene Raccolta",
+                icons: [
+                    {
+                        src: "/pwa-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                        purpose: "any",
+                    },
+                    {
+                        src: "/pwa-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "any",
+                    },
+                    {
+                        src: "/pwa-maskable-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                        purpose: "maskable",
+                    },
+                    {
+                        src: "/pwa-maskable-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "maskable",
+                    },
+                ],
+                start_url: "/",
+                display: "standalone",
+                background_color: "#FFFFFF",
+                theme_color: "#0284c7",
+                description: "Calendario della Raccolta Rifiuti.",
             },
         }),
     ],
